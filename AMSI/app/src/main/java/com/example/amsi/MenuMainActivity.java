4af57797a625a70721c81edb3e693ce3e80eaba3
eduemleiria <2230233@my.ipleiria.pt>
2025-01-18 23:38:21 +0000

    Commit final
    erro no menu main

--- AMSI/app/src/main/java/com/example/amsi/MenuMainActivity.java
+++ AMSI/app/src/main/java/com/example/amsi/MenuMainActivity.java
@@ -67,11 +67,11 @@ public class MenuMainActivity extends AppCompatActivity  implements NavigationVi
             Intent intent = new Intent(this, CarrinhoActivity.class);
             startActivity(intent);
             setTitle(item.getTitle());
-        }  else if (item.getItemId() == R.id.navLogout){
+        }  else if (item.getItemId() == R.id.navLogout) {
             Intent intent = new Intent(this, LoginActivity.class);
             startActivity(intent);
             setTitle(item.getTitle());
-
+        }
         if (fragment != null) {
             fragmentManager.beginTransaction().replace(R.id.contentFragment, fragment).commit();
         }
